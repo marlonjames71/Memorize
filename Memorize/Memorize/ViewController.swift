17f15596a03a474f9f844a8852697c1dd4290c96
Marlon Raskin <marlonjraskin@gmail.com>
2020-05-30 02:30:28 -0400

    (Added) Items populate tableView

--- Memorize/Memorize/ViewController.swift
+++ Memorize/Memorize/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+	var items = [MemoryItem]()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		loadItems()
+	}
+
+	func loadItems() {
+		guard let url = Bundle.main.url(forResource: "Items", withExtension: "json") else {
+			fatalError("Can't find JSON.")
+		}
+
+		guard let data = try? Data(contentsOf: url) else {
+			fatalError("Unable to load JSON.")
+		}
+
+		let decoder = JSONDecoder()
+
+		guard let savedItems = try? decoder.decode([MemoryItem].self, from: data) else {
+			fatalError("Failed to decode JSON.")
+		}
+
+		items = savedItems
 	}
 
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		items.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		let item = items[indexPath.row]
+		cell.textLabel?.text = item.title
+		cell.detailTextLabel?.text = item.text
+
+		return cell
+	}
 }
 
