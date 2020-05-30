a5759efa25b688d4155e9cfb2c9031810fae80c6
Marlon Raskin <marlonjraskin@gmail.com>
2020-05-30 01:57:32 -0400

    (Added) Model and json files

---
+++ Memorize/Memorize/MemoryItem.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryItem.swift
+//  Memorize
+//
+//  Created by Marlon Raskin on 5/30/20.
+//  Copyright © 2020 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryItem: Codable {
+	var title: String
+	var text: String
+}
